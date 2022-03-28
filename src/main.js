ffaaa0808c5be258c972092d5f33ca13e2d8e8b4
maury <maurymosqueda@gmail.com>
2022-03-28 15:23:55 -0300

    Add player and computer choice

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
 const CHOICES = ["rock", "paper", "scissors"];
 
+function computerPlay () {
+    return CHOICES[Math.floor(Math.random() * CHOICES.length)];
+}
+
+function userPlay() {
+    const userChoice = prompt('Please, enter: "Rock", "Paper" or "Scissors".').toLowerCase();
+    return userChoice;
+}
+
