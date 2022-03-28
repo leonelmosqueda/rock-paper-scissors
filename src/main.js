84d1aff759a170d125c005547db97e663f7f79eb
maury <maurymosqueda@gmail.com>
2022-03-28 16:13:29 -0300

    Add a round handler

--- src/main.js
+++ src/main.js
@@ -17,6 +17,8 @@ function userPlay() {
 function playRound (playerSelection, computerSelection) {
     playerSelection = userPlay();
     computerSelection = computerPlay();
+
+    const roundResult = checkRoundWinner(playerSelection, computerSelection);
 }
 
 function checkChoice(userChoice) {
@@ -30,6 +32,21 @@ function checkChoice(userChoice) {
         } else {
             return false;
         }
+    }
+}
+
+function checkRoundWinner (playerSelection, computerSelection) {
+    const MESSAGE_WIN = "You Win This Round!"
+    const MESSAGE_LOSE = "You Lose This Round!"
+
+    if (playerSelection === computerSelection) {
+        return "This round is a tie!"
+    } else if (playerSelection === CHOICES[0]) {
+        return computerSelection === CHOICES[1] ? `${MESSAGE_LOSE} ${computerSelection} beats ${playerSelection}` : `${MESSAGE_WIN} ${playerSelection} beats ${computerSelection}`
+    } else if (playerSelection === CHOICES[1]) {
+        return computerSelection === CHOICES[0] ? `${MESSAGE_WIN} ${playerSelection} beats ${computerSelection}` : `${MESSAGE_LOSE} ${computerSelection} beats ${playerSelection}`
+    } else {
+        return computerSelection === CHOICES[0] ? `${MESSAGE_LOSE} ${computerSelection} beats ${playerSelection}` : `${MESSAGE_WIN} ${playerSelection} beats ${computerSelection}`
     }
 }
 
