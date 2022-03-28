8d13c51b9031843b6843f61df065e7569edb9b28
maury <maurymosqueda@gmail.com>
2022-03-28 15:50:31 -0300

    Add an answer check

--- src/main.js
+++ src/main.js
@@ -5,7 +5,32 @@ function computerPlay () {
 }
 
 function userPlay() {
-    const userChoice = prompt('Please, enter: "Rock", "Paper" or "Scissors".').toLowerCase();
-    return userChoice;
+    const userChoice = prompt('Please, enter: "Rock", "Paper" or "Scissors".').toLowerCase().trim();
+    
+    if (checkChoice(userChoice)) {
+        return userChoice;
+    } else {
+        return console.log("Enter a correct choice!");
+    }
 }
 
+function playRound (playerSelection, computerSelection) {
+    playerSelection = userPlay();
+    computerSelection = computerPlay();
+}
+
+function checkChoice(userChoice) {
+    for (let i = 0; i < CHOICES.length; i++) {
+        const choice = CHOICES[i];
+
+        if (userChoice === choice) {
+            return true;
+        } else if (i < CHOICES.length - 1) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+}
+
+playRound();
