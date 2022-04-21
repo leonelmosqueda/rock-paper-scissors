9a0057e70be994febc4d1e0bd83c272706680aed
maury <maurymosqueda@gmail.com>
2022-04-21 20:50:40 -0300

    Add restart button

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 const CHOICES = ["rock", "paper", "scissors"];
-const $btnChoices = document.querySelectorAll('.choices button');
+const $btnChoices = document.querySelectorAll('.choices .choice');
 const $btnStart = document.querySelector('#start-game');
-let round = 0;
 
 let playerScore = 0;
 let machineScore = 0;
@@ -10,6 +9,7 @@ let machineScore = 0;
 $btnStart.addEventListener('click', startGame)
 
 function startGame() {
+    resetGame();
     enableDashboard();
     disableButtonStart()
 }
@@ -33,12 +33,8 @@ $btnChoices.forEach((choice) => {
     });
 });
 
-function userPlay(e) {
-    return e.target.id;
-}
-
-function computerPlay () {
-    return CHOICES[Math.floor(Math.random() * CHOICES.length)];
+function game(e) {
+    playRound(e);
 }
 
 function playRound (e) {
@@ -48,24 +44,28 @@ function playRound (e) {
     showSelections(playerSelection, machineSelection);
 
     const roundResult = checkRoundWinner(playerSelection, machineSelection);
+    highlightStatus(roundResult);
+
     updateScore(roundResult);
     showResultRound(roundResult);
 
     checkEndOfTheGame();
 }
 
-function checkChoice(userChoice) {
-    for (let i = 0; i < CHOICES.length; i++) {
-        const choice = CHOICES[i];
+function userPlay(e) {
+    return e.target.id;
+}
 
-        if (userChoice === choice) {
-            return true;
-        } else if (i < CHOICES.length - 1) {
-            continue;
-        } else {
-            return false;
-        }
-    }
+function computerPlay () {
+    return CHOICES[Math.floor(Math.random() * CHOICES.length)];
+}
+
+function showSelections (playerSelection, machineSelection) {
+    const $containerPlayerChoice = document.querySelector('#player-choice');
+    const $containerMachineChoice = document.querySelector('#machine-choice');
+
+    $containerPlayerChoice.textContent = playerSelection;
+    $containerMachineChoice.textContent = machineSelection;
 }
 
 function checkRoundWinner (playerSelection, computerSelection) {
@@ -96,16 +96,38 @@ function checkRoundWinner (playerSelection, computerSelection) {
     }
 }
 
-function game(e) {
-    playRound(e);
-}
+function highlightStatus (roundResult) {
+    const $playerSelection = document.querySelector('.player-choice');
+    const $machineSelection = document.querySelector('.machine-choice');
 
-function showSelections (playerSelection, machineSelection) {
-    const $containerPlayerChoice = document.querySelector('#player-choice');
-    const $containerMachineChoice = document.querySelector('#machine-choice');
-
-    $containerPlayerChoice.textContent = playerSelection;
-    $containerMachineChoice.textContent = machineSelection;
+    if (roundResult === "You Win This Round!") {
+        $playerSelection.classList.add('win-round');
+        setTimeout(() => {
+            $playerSelection.classList.remove('win-round');
+        }, 1500);
+        $machineSelection.classList.add('lose-round');
+        setTimeout(() => {
+            $machineSelection.classList.remove('lose-round');
+        }, 1500);
+    } else if (roundResult === "You Lose This Round!") {
+        $playerSelection.classList.add('lose-round');
+        setTimeout(() => {
+            $playerSelection.classList.remove('lose-round');
+        }, 1500);
+        $machineSelection.classList.add('win-round');
+        setTimeout(() => {
+            $machineSelection.classList.remove('win-round');
+        }, 1500);
+    } else {
+        $playerSelection.classList.add('tie-round');
+        setTimeout(() => {
+            $playerSelection.classList.remove('tie-round');
+        }, 1500);
+        $machineSelection.classList.add('tie-round');
+        setTimeout(() => {
+            $machineSelection.classList.remove('tie-round');
+        }, 1500);
+    }
 }
 
 function updateScore(roundResult) {
@@ -117,6 +139,9 @@ function updateScore(roundResult) {
         $playerScore.textContent = playerScore.toString().padStart(2, '0');
     } else if (roundResult === "You Lose This Round!") {
         machineScore++;
+        $machineScore.textContent = machineScore.toString().padStart(2, '0');
+    } else if (roundResult === 0) {
+        $playerScore.textContent = playerScore.toString().padStart(2, '0');
         $machineScore.textContent = machineScore.toString().padStart(2, '0');
     } else {
         return;
@@ -134,10 +159,14 @@ function checkEndOfTheGame() {
 
     if (playerScore === 5) {
         showWinMessage($resultGame);
-        disabledBoard();        
+        disableDashboard();
+        changeTextButton();
+        enableButtonStart();      
     } else if (machineScore === 5) {
         showDefeatMessage($resultGame);
-        disabledBoard();
+        disableDashboard();
+        changeTextButton();
+        enableButtonStart();
     } else {
         return;
     }
@@ -151,8 +180,28 @@ function showDefeatMessage (container) {
     container.textContent = "Sorry! You Lost This Game!"
 }
 
-function disabledBoard () {
-    $btnChoices.forEach(btn => {
-        btn.classList.add('disabled');
-    })
+function disableDashboard () {
+    const $dashboard = document.querySelectorAll('.choices .container-frame');
+
+    $dashboard.forEach((option) => {option.classList.add('disabled')});
+}
+
+function changeTextButton () {
+    const $button = document.querySelector('#start-game');
+
+    $button.textContent = 'Restart game';
+}
+
+function enableButtonStart () {
+    const $buttonStart = document.querySelector('#start-game');
+
+    $buttonStart.classList.remove('disabled');
+}
+
+function resetGame () {
+    playerScore = 0;
+    machineScore = 0;
+
+    updateScore(0);
+    showSelections('', '');
 }
